bbafd227b76404912d4fc58793ff387791c3bc2b
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2022-04-15 11:15:32 -0300

    add: adiciona animate.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Intro from './components/Intro';
 import Projects from './components/Projects';
 import './App.css';
+import 'animate.css';
 
 function App() {
   return (
